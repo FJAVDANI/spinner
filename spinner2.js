344aa7322b18bbdd5e05f518962c5c90f570204d
FJAVDANI <115185399+FJAVDANI@users.noreply.github.com>
2023-07-08 02:28:23 +0530

    Update spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,1 +1,17 @@
+const spinnerChars = ['|', '/', '-', '\\'];
+let index = 0;
+
+const spinner = setInterval(() => {
+  const delay = [100, 300, 500, 700];
+  const char = spinnerChars[index];
+  
+  process.stdout.write(`\r${char}   `);
+  
+  index = (index + 1) % spinnerChars.length;
+  
+  setTimeout(() => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+  }, delay[index]);
+}, 100);
 
